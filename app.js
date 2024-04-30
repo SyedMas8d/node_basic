cc838bee189ae11dba5eb8d4a35a5e1bc2d8bdc4
SyedMas8d <99374650+SyedMas8d@users.noreply.github.com>
2024-04-30 07:09:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// It will parese the request body (So we can fetch req data req.body)
+app.use(express.json());
+
 // Create Server In Any port
 app.listen(3000, () => {
     console.log('Server Create In 3000 Port')
@@ -11,5 +14,16 @@ app.get('/users', (req, res) => {
 
     return res.status(200).json({
         message: 'Success'
+    })
+})
+
+app.get('/data', (req, res) => {
+
+    return res.status(200).json({
+        message: 'Success',
+        data: {
+            query: req.query,
+            body: req.body
+        }
     })
 })
